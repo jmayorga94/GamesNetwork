bfa2037eb42172d0b5542dafd6a4bfe520c100cf
jmayorga94 <javi.mayorga1994@gmail.com>
2021-02-08 00:10:07 -0600

    Refactor: Change IGenericRepository generic type

--- GamesNetwork.Domain/Commons/IGenericRepository.cs
+++ GamesNetwork.Domain/Commons/IGenericRepository.cs
@@ -5,12 +5,10 @@ using System.Text;
 
 namespace GamesNetwork.Domain.Commons
 {
-    public interface IGenericRepository<TEntity> where TEntity : class
+    public interface IGenericRepository<TEntity> 
     {
-
         IEnumerable<TEntity> FindAll();
         TEntity FindById(int id);
-
         IEnumerable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate);
         void Insert(TEntity entity);
         void Update(TEntity entity);
